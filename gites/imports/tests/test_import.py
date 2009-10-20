2a515dff3fbd199493179551c89ff951038ccf4d
Jean-François Roche <jfroche@affinitic.be>
2009-10-20 16:17:29 +0000

    initial import, tests only

---
+++ gites/imports/tests/test_import.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+gites.import
+
+Licensed under the GPL license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+
+$Id: event.py 67630 2006-04-27 00:54:03Z jfroche $
+"""
+from Products.CMFPlone.tests.PloneTestCase import FunctionalTestCase
+from zope.app.component.hooks import setHooks
+from Products.PloneTestCase.layer import PloneSite, onsetup
+from Testing import ZopeTestCase as ztc
+from Products.PloneTestCase import PloneTestCase as ptc
+import unittest
+from zope.testing import doctest, cleanup
+from Products.Five import zcml
+import tempfile
+import shutil
+import os
+from collective.transmogrifier.transmogrifier import configuration_registry
+
+
+BASEDIR = None
+
+
+def registerConfig(name, configuration):
+    filename = os.path.join(BASEDIR, '%s.cfg' % name)
+    open(filename, 'w').write(configuration)
+    configuration_registry.registerConfiguration(
+        name,
+        u"Pipeline configuration '%s' from "
+        u"'collective.transmogrifier.tests'" % name,
+        u'', filename)
+
+
+from Testing.ZopeTestCase import FunctionalDocFileSuite as Suite
+
+UNITTESTS = []
+
+OPTIONFLAGS = (doctest.ELLIPSIS |
+               doctest.NORMALIZE_WHITESPACE |
+               doctest.REPORT_ONLY_FIRST_FAILURE)
+
+linguaPloneHook = """
+<configure
+    xmlns="http://namespaces.zope.org/zope"
+    xmlns:genericsetup="http://namespaces.zope.org/genericsetup"
+    xmlns:five="http://namespaces.zope.org/five">
+
+  <five:registerPackage package="Products.LinguaPlone"
+                        initialize="Products.LinguaPlone.initialize" />
+
+</configure>
+"""
+
+
+@onsetup
+def setupPackages():
+    import Products.LinguaPlone
+    zcml.load_string(linguaPloneHook)
+    zcml.load_config('configure.zcml', Products.LinguaPlone)
+    import gites.imports
+    zcml.load_config('configure.zcml', gites.imports)
+    ztc.installPackage('gites.imports')
+    ztc.installPackage('Products.LinguaPlone')
+
+setupPackages()
+ptc.setupPloneSite(products=['gites.imports',
+                             'Products.LinguaPlone'])
+
+
+class GitesImportZCMLLayer(PloneSite):
+
+    @classmethod
+    def setUp(cls):
+        import Products
+        zcml.load_config('meta.zcml', Products.Five)
+        zcml.load_config('event.zcml', Products.Five)
+        zcml.load_config('configure.zcml', Products.Five)
+        setHooks()
+        import Products.LinguaPlone
+        zcml.load_string(linguaPloneHook)
+        zcml.load_config('configure.zcml', Products.LinguaPlone)
+        global BASEDIR
+        BASEDIR = tempfile.mkdtemp('transmogrifierTestConfigs')
+        import plone.app.transmogrifier
+        import Products.Five
+        zcml.load_config('configure.zcml', Products.Five)
+        import Products.GenericSetup
+        zcml.load_config('meta.zcml', Products.GenericSetup)
+        zcml.load_config('configure.zcml', plone.app.transmogrifier)
+        import gites.imports
+        zcml.load_config('configure.zcml', gites.imports)
+
+    @classmethod
+    def tearDown(cls):
+        shutil.rmtree(BASEDIR)
+        cleanup.cleanUp()
+
+
+class GitesImportFunctionalTestCase(FunctionalTestCase):
+    layer = GitesImportZCMLLayer
+
+
+def test_suite():
+    suites = [Suite('../gitesimport.txt',
+               optionflags=OPTIONFLAGS,
+               package='gites.imports.tests',
+               test_class=GitesImportFunctionalTestCase)]
+    return unittest.TestSuite(suites)
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='gites.imports',
+      version=version,
+      description="Import Content for Gites using transmogrifier",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['gites'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.transmogrifier',
+          'collective.blueprint.translationlinker',
+          'collective.blueprint.downloader',
+          'Plone',
+          'Products.LinguaPlone'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
